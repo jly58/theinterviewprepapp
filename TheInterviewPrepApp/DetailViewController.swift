0463bf298039295faa1d3b2dce44a977706ca294
jly58 <lyjes@oregonstate.edu>
2023-11-16 21:26:01 -0800

    let user type answer

--- TheInterviewPrepApp/DetailViewController.swift
+++ TheInterviewPrepApp/DetailViewController.swift
@@ -4,26 +4,40 @@
 //
 //  Created by Jessica Ly on 11/15/23.
 //
-
 import UIKit
 
 class DetailViewController: UIViewController {
-
+    
+    @IBOutlet weak var answerTextView: UITextView!
+    
+    var interviewAnswer: String?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        // Set up initial text or any other configurations
+        answerTextView.text = interviewAnswer ?? "Type your answer here..."
+        answerTextView.delegate = self
     }
     
-
-    /*
     // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    @IBAction func saveButtonTapped(_ sender: UIBarButtonItem) {
+        // Save the answer and go back to the previous view controller
+        interviewAnswer = answerTextView.text
+        navigationController?.popViewController(animated: true)
     }
-    */
-
 }
+    extension DetailViewController: UITextViewDelegate {
+        func textViewDidBeginEditing(_ textView: UITextView) {
+            if textView.text == "Type your answer here..." {
+                textView.text = ""
+            }
+        }
+        
+        func textViewDidEndEditing(_ textView: UITextView) {
+            // Save the text when the user finishes editing
+            // You can save it to a property, database, or wherever you need
+        }
+    }
+
